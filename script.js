e1b1fd0f7c285cb1ef35ef691a441500caf56ba3
Kleijn95 <kleijn95@gmail.com>
2025-01-17 11:33:56 +0100

    fist page "done"

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-document.getElementById("searchToggle").addEventListener("click", function () {
-  const searchForm = document.getElementById("searchForm");
-  if (searchForm.classList.contains("d-none")) {
-    searchForm.classList.remove("d-none");
-    searchForm.classList.add("d-flex");
-  } else {
-    searchForm.classList.remove("d-flex");
-    searchForm.classList.add("d-none");
-  }
-});
+if (window.location.pathname === "/index.html") {
+  document.getElementById("searchToggle").addEventListener("click", function () {
+    const searchForm = document.getElementById("searchForm");
+    if (searchForm.classList.contains("d-none")) {
+      searchForm.classList.remove("d-none");
+      searchForm.classList.add("d-flex");
+    } else {
+      searchForm.classList.remove("d-flex");
+      searchForm.classList.add("d-none");
+    }
+  });
+}
