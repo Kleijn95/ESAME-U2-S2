a12980e48b1b41588abe8b7ea717280522c72224
Kleijn95 <kleijn95@gmail.com>
2025-01-17 10:44:05 +0100

    navbar done

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("searchToggle").addEventListener("click", function () {
+  const searchForm = document.getElementById("searchForm");
+  if (searchForm.classList.contains("d-none")) {
+    searchForm.classList.remove("d-none");
+    searchForm.classList.add("d-flex");
+  } else {
+    searchForm.classList.remove("d-flex");
+    searchForm.classList.add("d-none");
+  }
+});
